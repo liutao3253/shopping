39a6be3989633d18c38d98c49a3b2a4760f6da8c
liutao3253 <1937373367@qq.com>
2018-01-19 18:51:10 +0800

    添加后台数据

---
+++ utils/utils.js
@@ -0,0 +1,12 @@
+
+var CONFIG = {
+  API_URL:{
+    // 轮播
+    BANNER_QUERY:'https://ifanr.in/api/v1.4/campaign_banner/',
+    // 分类
+    CATEGORY_QUERY:'https://ifanr.in/api/v1.4/category/',
+    SHELF_QUERY:'https://ifanr.in/api/v1.4/shelf/',
+    PRODUCT_LIST: 'https://ifanr.in/api/v1.4/shelf/'
+  }
+}
+module.exports = CONFIG
